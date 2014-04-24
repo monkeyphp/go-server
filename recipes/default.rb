7f81db48108856f814cc97e27e8dd784d5116fc3
David White <david@monkeyphp.com>
2014-04-24 21:55:11 +0100

    Updated version of go

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,14 +16,13 @@ package "unzip" do
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/go-server.deb" do
-    source "http://download01.thoughtworks.com/go/13.4.1/ga/go-server-13.4.1-18342.deb"
-    mode 0777
+    source "http://download01.thoughtworks.com/go/14.1.0/ga/go-server-14.1.0-18882.deb"
+    mode "0777"
 end
 
-package "go-server" do
-    source "#{Chef::Config[:file_cache_path]}/go-server.deb"
-    provider Chef::Provider::Package::Dpkg
-    action :install
+execute "install_go-server" do
+    command "sudo dpkg -i #{Chef::Config[:file_cache_path]}/go-server.deb" 
+    action :run
 end
 
 service "go-server" do
@@ -31,13 +30,6 @@ service "go-server" do
     action :nothing
 end
 
-template "/etc/default/go-server" do
-    source   "go-server.erb"
-    owner    "root"
-    group    "root"
-    mode     0644
-    notifies :restart, resources(:service => "go-server")
-end
 
 # http://www.thoughtworks.com/products/docs/go/13.4/help/
 
